0e8f5a2c989a0002371634766ffec446725b1c62
Karen MacPherson <karen@fullstackacademy.com>
2016-08-21 12:22:11 -0400

    initial commit

---
+++ GuessingGame.js
@@ -0,0 +1,72 @@
+
+var Game = function() {
+    console.log('in game');
+    this.playersGuess = null;
+    this.winningNumber = generateWinningNumber();
+    this.pastGuesses = [];
+}
+
+function generateWinningNumber() {
+    return Math.ceil(Math.random()*100);
+}
+
+
+function playAgain() {
+    return new Game(); //check that old game !== new game
+}
+
+Game.prototype.difference = function() {
+    return Math.abs(this.playersGuess-this.winningNumber);
+}
+
+Game.prototype.isLower = function() {
+    return this.playersGuess < this.winningNumber;
+}
+
+Game.prototype.playersGuessSubmission = function(guess) {
+    if(typeof guess !== 'number' || guess < 1 || guess > 100) {
+        throw "That is an invalid guess.";
+    }
+    this.playersGuess = guess;
+    return this.checkGuess();
+}
+
+Game.prototype.checkGuess = function() {
+    console.log('in check guess');
+    if(this.playersGuess===this.winningNumber) {
+        return 'You Win!'
+    }
+    else {
+        if(this.pastGuesses.indexOf(this.playersGuess) > -1) {
+            return 'You have already guessed that number.';
+        }
+        else {
+            this.pastGuesses.push(this.playersGuess);
+            if(this.pastGuesses.length === 5) {
+                return 'You Lose.';
+            }
+            else {
+                var diff = this.difference();
+                if(diff < 10) return'You\'re burning up!';
+                else if(diff < 25) return'You\'re lukewarm.';
+                else if(diff < 50) return'You\'re a bit chilly.';
+                else return'You\'re ice cold!';
+            } 
+        }
+    }
+}
+
+Game.prototype.provideHint = function() {
+    var hintArray = [this.winningNumber, generateWinningNumber(), generateWinningNumber()];
+    return shuffle(hintArray);
+}
+
+function shuffle(arr) { //Fisher-Yates - https://bost.ocks.org/mike/shuffle/
+   for(var i = arr.length-1; i > 0; i--) {
+       var randomIndex = Math.floor(Math.random() * (i + 1));
+       var temp = arr[i];
+       arr[i] = arr[randomIndex];
+       arr[randomIndex] = temp;
+    }
+    return arr;
+}
