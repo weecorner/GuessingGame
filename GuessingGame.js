7f65950d44623f3f00c9e4bb1e0bab89de0c86bd
Jing Wang <jwang2048@gmail.com>
2017-04-16 02:11:44 -0400

    finished guessing game part 2

---
+++ GuessingGame.js
@@ -0,0 +1,76 @@
+var Game = function(){
+	this.playersGuess = null;
+	this.winningNumber = generateWinningNumber();
+	this.pastGuesses = [];
+};
+
+function generateWinningNumber(){
+	return Math.floor(Math.random()*100 + 1);
+}
+
+function newGame(){
+	return new Game();
+}
+
+Game.prototype.difference = function(){
+	return Math.abs(this.playersGuess - this.winningNumber);
+}
+
+Game.prototype.isLower = function(){
+	return this.playersGuess < this.winningNumber;
+}
+
+Game.prototype.playersGuessSubmission = function(guess){
+	if (typeof guess !== 'number' || guess < 1 || guess > 100) {
+		throw "That is an invalid guess.";
+	}
+	this.playersGuess = guess;
+	return this.checkGuess();
+}
+
+Game.prototype.checkGuess = function(){
+	if (this.playersGuess === this.winningNumber) {
+		return "You Win!"
+	} else {
+		if (this.pastGuesses.indexOf(this.playersGuess) > -1) {
+			return "You have already guessed that number.";
+		} else {
+			this.pastGuesses.push(this.playersGuess);
+			if(this.pastGuesses.length === 5) {
+				return "You Lose.";
+			}
+			else {
+				/*var diff = this.difference();
+				if (diff < 10) {
+					return "You\'re burning up!";
+				} else if (diff < 25) {
+					return "You\'re lukewarm.";
+				} else if (diff < 50) {
+					return "You\'re a bit chilly.";
+				} else {
+					return "You\'re ice cold!";
+				}*/
+				var diff = this.difference();
+                if(diff < 10) return'You\'re burning up!';
+                else if(diff < 25) return'You\'re lukewarm.';
+                else if(diff < 50) return'You\'re a bit chilly.';
+                else return'You\'re ice cold!';
+			}
+		}
+	}
+}
+
+Game.prototype.provideHint = function(){
+	var hintArray = [this.winningNumber, generateWinningNumber(), generateWinningNumber()];
+	return shuffle(hintArray);
+}
+
+function shuffle(arr) {
+	for (var i = arr.length - 1; i > 0; i--) {
+		var randomIndex = Math.floor(Math.random() * (i + 1));
+		var temp = arr[i];
+		arr[i] = arr[randomIndex];
+		arr[randomIndex] = temp;
+	}
+	return arr;
+}
